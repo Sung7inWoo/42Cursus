2d98beefb648efd796a8cc384921408b7f2e4f2c
Sung7inWoo <bigjirut@gmail.com>
2023-02-25 23:12:32 +0700

    fix strjoin

--- Level1/ft_strjoin.c
+++ Level1/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jthanikp <jthanikp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 14:52:40 by jthanikp          #+#    #+#             */
-/*   Updated: 2023/02/25 21:59:55 by jthanikp         ###   ########.fr       */
+/*   Updated: 2023/02/25 23:06:57 by jthanikp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) * sizeof(char));
+	ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!(ptr))
 		return (NULL);
 	ft_strlcpy(ptr, s1, ft_strlen(s1) + 1);
